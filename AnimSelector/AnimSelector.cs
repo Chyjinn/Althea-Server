a9f50b7da89009bce3b0dca45f36a5303a7ee273
morcsog <morcsog99@gmail.com>
2023-10-25 09:27:50 +0200

    Anim selector
    selector

---
+++ AnimSelector/AnimSelector.cs
@@ -0,0 +1,17 @@
+﻿using GTANetworkAPI;
+using GTANetworkMethods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.AnimSelector
+{
+    internal class AnimSelector : Script
+    {
+        [Command("animselector")]
+        public void animSelector(GTANetworkAPI.Player player)
+        {
+            player.TriggerEvent("toggleSelectorWindow");
+        }
+    }
+}
